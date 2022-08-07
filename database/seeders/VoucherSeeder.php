1921143b9234c0bc506b81ffcf53815ed2661256
mufidmoesz <mufidmusyaffa@365.telkomuniversity.ac.id>
2022-08-07 20:30:04 +0700

    vouchercontroller

--- database/seeders/VoucherSeeder.php
+++ database/seeders/VoucherSeeder.php
@@ -19,27 +19,27 @@ class VoucherSeeder extends Seeder
         voucher::create([
             'nama' => 'voucher1',
             'kode' => 'abc1234',
-            'masa_berlaku' => '2022-08-06 22:59:00'
+            'masa_berlaku' => '2022-08-06'
         ]);
         voucher::create([
             'nama' => 'voucher2',
             'kode' => 'def5678',
-            'masa_berlaku' => '2022-08-06 23:59:00'
+            'masa_berlaku' => '2022-08-06'
         ]);
         voucher::create([
             'nama' => 'voucher3',
             'kode' => 'ghi9012',
-            'masa_berlaku' => '2022-08-06 22:59:00'
+            'masa_berlaku' => '2022-08-07'
         ]);
         voucher::create([
             'nama' => 'voucher4',
             'kode' => 'jkl3456',
-            'masa_berlaku' => '2022-08-07 00:00:00'
+            'masa_berlaku' => '2022-08-07'
         ]);
         voucher::create([
             'nama' => 'voucher5',
             'kode' => 'mno6789',
-            'masa_berlaku' => '2022-08-07 02:00:00'
+            'masa_berlaku' => '2022-08-07'
         ]);
     }
 }
